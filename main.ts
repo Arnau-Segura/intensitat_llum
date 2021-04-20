4ce263acb54ecb8d65b8d0f6993761b607c117e9
Arnau-Segura <82875788+Arnau-Segura@users.noreply.github.com>
2021-04-20 12:41:00 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,43 @@
+basic.showLeds(`
+    # # . # #
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+let llum = 0
+led.setBrightness(llum)
+serial.setBaudRate(BaudRate.BaudRate115200)
 basic.forever(function () {
-	
+    if (llum > 5) {
+        basic.showLeds(`
+            # # . # #
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        llum += 1
+        led.setBrightness(llum)
+        serial.writeLine("" + (llum))
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        llum += -1
+        led.setBrightness(llum)
+        serial.writeLine("" + (llum))
+    }
 })
